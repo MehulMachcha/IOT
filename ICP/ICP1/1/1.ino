f9f2f40d3319114cdebb016498d69553019ea67e
Yong <yongzheng0809@gmail.com>
2019-01-25 19:18:28 -0600

    ICP1

---
+++ ICP/ICP1/1/1.ino
@@ -0,0 +1,36 @@
+int switchState = 0;
+
+void setup()
+{
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+}
+
+void loop()
+{
+  switchState = digitalRead(2);
+  
+  if (switchState == LOW)
+  {
+    // The button is not pressed
+    digitalWrite(3, HIGH); // Green LED
+    digitalWrite(4, LOW); // Red LED
+    digitalWrite(5, LOW); // Red LED
+  }
+  else
+  {
+    for(int l=0; l<=5; l++){
+      digitalWrite(3, LOW);
+      digitalWrite(4, LOW);
+      digitalWrite(5, HIGH);
+
+      delay(250);
+      digitalWrite(4, HIGH);
+      digitalWrite(5, LOW);
+      delay(250);
+    }
+  }
+}
+
